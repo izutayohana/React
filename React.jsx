a39979d4127380a8afe76453cd106da6d637a8b4
DESKTOP-GPM9SNA\Yohana <yohanaizuta@gmail.com>
2023-07-31 18:51:36 +0900

    React ES6 class syntax and stateless component

--- React.jsx
+++ React.jsx
@@ -5,3 +5,34 @@ const JSX = (
       {/*comment*/}
     </div>
   );
+
+
+  {/*ReactDOM offers a simple method to render React elements to the DOM which looks like this: ReactDOM.render(componentToRender, targetNode), where the first argument is the React element or component that you want to render, and the second argument is the DOM node that you want to render the component to.
+
+As you would expect, ReactDOM.render() must be called after the JSX element declarations, just like how you must declare variables before using them.
+
+-----------------------------------
+
+One key difference in JSX is that you can no longer use the word class to define HTML classes. This is because class is a reserved word in JavaScript. Instead, JSX uses className.
+
+-------------------------------------
+Any JSX element can be written with a self-closing tag, and every element must be closed. The line-break tag, for example, must always be written as <br /> in order to be valid JSX that can be transpiled. A <div>, on the other hand, can be written as <div /> or <div></div>. The difference is that in the first syntax version there is no way to include anything in the <div />. 
+
+-------------------------------------
+here are two ways to create a React component. The first way is to use a JavaScript function. Defining a component in this way creates a stateless functional component. 
+ For now, think of a stateless component as one that can receive data and render it, but does not manage or track changes to that data. 
+
+ The other way to define a React component is with the ES6 class syntax. In the following example, Kitten extends React.Component:
+
+class Kitten extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+}
+*/}
